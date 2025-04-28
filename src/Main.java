3265f3ce3810236d2b91846f5746e6af123d35af
jamileth-kumpanam <jamilethkumpanam@MacBook-Air-de-Jamileth.local>
2025-04-27 19:33:43 -0500

    Proyecto

--- src/Main.java
+++ src/Main.java
@@ -197,7 +197,7 @@ public class Main {
         }
 
         solicitudes.add(solicitud);
-        System.out.println("✅ Solicitud registrada exitosamente!");
+        System.out.println("Solicitud registrada exitosamente!");
     }
 
     private static void listarProveedores() {
